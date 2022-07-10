40b912fd15c45b78cadceeaa0dc6e519aba7f4ac
Vadim Shabinevich <shabinevich.v@mxm.email>
2022-07-10 18:03:20 +0300

    .

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,24 +1,27 @@
-import { connect } from "react-redux";
-import * as actions from '../actions'
+import { inc, dec, rnd } from '../actions'
+import { useSelector, useDispatch } from "react-redux";
 
-const Counter = ({ counter, inc, dec, rnd, }) => {
+const Counter = () => {
+
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
   return (
     <div className="jumbotron">
       <h1>{counter}</h1>
-      <button onClick={dec} class="btn btn-primary">DEC</button>
-      <button onClick={inc} class="btn btn-primary">INC</button>
-      <button onClick={rnd} class="btn btn-primary">RND</button>
+      <button onClick={() => dispatch(dec())} class="btn btn-primary">DEC</button>
+      <button onClick={() => dispatch(inc())} class="btn btn-primary">INC</button>
+      <button onClick={() => dispatch(rnd())} class="btn btn-primary">RND</button>
     </div>
-
-  )
+  ) 
 }
 
-const mapStateToProps = (state) => {
-  return (
-    {
-      counter: state.value
-    }
-  )
-}
+// const mapStateToProps = (state) => {
+//   return (
+//     {
+//       counter: state.value
+//     }
+//   )
+// }
 
-export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
